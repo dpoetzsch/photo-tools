6e02e86977e40a81895344ed16cef663d0a27031
David Poetzsch-Heffter <davidpoetzsch@web.de>
2019-05-06 01:39:16 +0200

    improved dup finder

--- find-visual-duplicates.rb
+++ find-visual-duplicates.rb
@@ -1,21 +1,27 @@
 #!/usr/bin/env ruby
 
 require "yaml"
+require "dhash-vips"
+require "fileutils"
 
-if ARGV.length < 1
-  puts "Usage: find-visual-duplicates.rb <dbfile.yaml> <dhash | idhash>"
+if ARGV.length < 2
+  puts "Usage: find-visual-duplicates.rb <dbfile.yaml> <dhash | idhash> [prepare]"
   exit 1
 end
 
 ALGO = ARGV[1]
 
-db = YAML.load(File.read(arg))
+db = YAML.load(File.read(ARGV[0]))
 
 db.each do |k,v|
   db.delete(k) unless File.exists? k
 end
 
-db.to_a.each_with_index do |v, i|
+db = db.to_a
+
+Dir.mkdir("/tmp/duplicates") if ARGV[2] == "prepare"
+
+db.each_with_index do |v, i|
   f = v[0]
   h = v[1][ALGO]
 
@@ -24,15 +30,23 @@ db.to_a.each_with_index do |v, i|
   (i+1).upto(db.length - 1).each do |j|
     h2 = db[j][1][ALGO]
     diff = false
-    similar = DHashVips::DHash.hamming(h, h2) < 10 if ALGO == "dhash"
+    similar = DHashVips::DHash.hamming(h, h2) < 1 if ALGO == "dhash"
     similar = DHashVips::IDHash.distance(h, h2) < 15 if ALGO == "idhash"
     dups.push(db[j][0]) if similar
   end
 
   if dups.length > 0
-    puts "Potential duplicates:"
+    puts "Duplicates:"
     puts f
     puts dups
     puts
+
+    if ARGV[2] == "prepare"
+      Dir.mkdir "/tmp/duplicates/#{i}"
+      FileUtils.cp(f, "/tmp/duplicates/#{i}/")
+      dups.each do |d|
+        FileUtils.cp(d, "/tmp/duplicates/#{i}/")
+      end
+    end
   end
 end
