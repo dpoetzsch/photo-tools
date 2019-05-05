59cec0e81f238027100a2bf64ccb7613654abaaa
David Poetzsch-Heffter <davidpoetzsch@web.de>
2019-05-06 00:40:28 +0200

    added more hashes + update mechanism for hashdb

--- create-hashdb.rb
+++ create-hashdb.rb
@@ -2,18 +2,34 @@
 
 require "yaml"
 require "digest"
+require "dhash-vips"
 
 if ARGV.length < 2
   puts "Usage: create-hashdb.rb <folder> <dbfile.yaml>"
+  puts "If the db file already exists, it will only be updated."
   exit 1
 end
 
-db = {}
+if File.exists? ARGV[1]
+  db = YAML.load(File.read(ARGV[1]))
+else
+  db = {}
+end
 
 files = Dir[ARGV[0] + "/**/*"].find_all { |f| File.file? f }
 files.each_with_index do |f,i|
   printf("%5d/%5d: %s\n", i+1, files.length, f)
-  db[File.expand_path(f)] = Digest::SHA2.hexdigest(File.read(f)),
+
+  mtime = File.mtime(f).to_f
+  path = File.expand_path(f)
+  if db[path].nil? || db[path]["mtime"] != mtime
+    db[path] = {
+      "sha" => Digest::SHA2.hexdigest(File.read(f)),
+      "dhash" => DHashVips::DHash.calculate(f),
+      "idhash" => DHashVips::IDHash.fingerprint(f),
+      "mtime" => File.mtime(f).to_f
+    }
+  end
 end
 
 # write db
