70b73170c34885e29fc70c4b40d39a08291d20f7
David Poetzsch-Heffter <davidpoetzsch@web.de>
2019-05-06 11:33:13 +0200

    better options for script

--- create-hashdb.rb
+++ create-hashdb.rb
@@ -5,13 +5,15 @@ require "digest"
 require "dhash-vips"
 
 if ARGV.length < 2
-  puts "Usage: create-hashdb.rb <folder> <dbfile.yaml>"
+  puts "Usage: create-hashdb.rb <dbfile.yaml> <folder>*"
   puts "If the db file already exists, it will only be updated."
   exit 1
 end
 
-if File.exist? ARGV[1] # rubocop:disable Style/ConditionalAssignment
-  db = YAML.load(File.read(ARGV[1]))
+HASHDB = ARGV[0]
+
+if File.exist? HASHDB # rubocop:disable Style/ConditionalAssignment
+  db = YAML.load(File.read(HASHDB))
 else
   db = {}
 end
@@ -21,8 +23,12 @@ db.each do |k,v|
   db.delete(k) unless File.exist? k
 end
 
-files = Dir[ARGV[0] + "/**/*"].find_all { |f| File.file? f }
-files.each_with_index do |f,i|
+files = ARGV[1..-1].map { |a| Dir[a] + "/**/*"] }
+  .flatten
+  .find_all { |f| File.file? f }
+  .sort
+
+files.each_with_index do |f, i|
   begin
     printf("%5d/%5d: %s\n", i + 1, files.length, f)
 
@@ -50,4 +56,4 @@ files.each_with_index do |f,i|
 end
 
 # write db
-File.open(ARGV[1], 'w') { |f| f.write YAML.dump(db) }
+File.open(HASHDB, 'w') { |f| f.write YAML.dump(db) }
