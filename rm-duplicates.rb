b5eaf0df6bb5ee566a8f1ba860a472a51d3669cd
David Poetzsch-Heffter <git@poehe.de>
2020-11-29 13:35:27 +0100

    updated rm-duplicates script to new yaml output format

--- rm-duplicates.rb
+++ rm-duplicates.rb
@@ -1,18 +1,16 @@
 #!/usr/bin/env ruby
 
+require "yaml"
 require "fileutils"
 
 if ARGV.length < 1
-  puts "Usage: rm-duplicates <duplicates-output-file.txt> [rm]"
+  puts "Usage: rm-duplicates <duplicates-output-file.yaml> [rm]"
   exit 1
 end
 
-dups = File.read(ARGV[0]).split("\n\nDuplicates:\n")
-dups[0] = dups[0].split("\n")[1..-1].join("\n")
-
-dups.map! { |d| d.split("\n")[1..-1] }
-
-dups.flatten.each { |f| 
-  puts f
-  FileUtils.rm(f) if ARGV[1] == "rm"
-}
+YAML.load(File.read(ARGV[0])).each do |k, dups|
+  dups[1..-1].each do |f|
+    puts f
+    FileUtils.rm(f) if ARGV[1] == "rm"
+  end
+end
